e0edd7863f6dd9202a81530eb8d844db256056d1
lizhencn <lizhencn@163.com>
2020-03-04 10:20:01 +0800

    channel select

--- set_ca_analyzer_pane.py
+++ set_ca_analyzer_pane.py
@@ -16,31 +16,28 @@ class SetAnalyzer(QWidget, Ui_Form):
         self.mxt_signal_type = None
         self.xt_signal_type = None
         self.analyze_mode = None
+        self.channel_mode = None
         self.channels = []
 
     def select_data_type(self, current_no):
         self.data_type = self.sender().tabText(current_no)
-        print(self.data_type)
-        print("mxt_signal_type", self.mxt_signal_type)
-        print("xt_signal_type", self.xt_signal_type)
 
     def select_xt_signal_type(self):
         self.xt_signal_type = self.sender().text()
-        print(self.xt_signal_type)
-        print(self.data_type)
 
     def select_mxt_signal_type(self):
         self.mxt_signal_type = self.sender().text()
-        print(self.mxt_signal_type)
-        print(self.data_type)
 
     def select_all_channel(self):
+        self.channel_mode = self.sender().text()
         self.change_channel(False, True)
 
     def manual_by_files(self):
+        self.channel_mode = self.sender().text()
         self.change_channel(False, False)
 
     def specified_by_laser(self):
+        self.channel_mode = self.sender().text()
         self.change_channel(True)
 
     def change_channel(self, enable, checked=False):
@@ -67,26 +64,37 @@ class SetAnalyzer(QWidget, Ui_Form):
 
     def select_analyze_mode(self):
         self.analyze_mode = self.sender().text()
-        print(type(self.sender().text()))
-        print(self.sender().text())
 
     def select_channel(self, checked):
         if checked:
             self.channels.append(self.sender().text())
-            print(self.channels)
         else:
             self.channels.remove(self.sender().text())
-            print(self.channels)
 
     def execute_analyzer(self, files):
         self.files = files
         self.files_num = len(files)
-        print(self.files)
-        print(self.files_num)
-        print(self.file_no)
-        self.open_analyzer_signal.emit(self.files, self.files_num, self.file_no, self.data_type, self.signal_type, self.analyze_mode, self.channels)
+        self.open_analyzer_signal.emit(self.files, self.files_num, self.file_no, self.data_type, self.signal_type,
+                                       self.analyze_mode, self.channels)
 
     def open_file(self):
+        if (self.data_type == "XT" and self.xt_signal_type is None) or (
+                self.data_type == "mXT" and self.mxt_signal_type is None):
+            print("Please Select Data Type!")
+            return
+        if self.analyze_mode is None:
+            print("Please Select Analyze Mode!")
+            return
+        if self.channel_mode is None:
+            print("Please Select Channel Mode!")
+            return
+        if self.channel_mode == "Specified by laser" and len(self.channels) == 0:
+            print("Please Select Channel Laser!")
+            return
+        if self.data_type == "XT" and self.xt_signal_type is not None:
+            self.signal_type = self.xt_signal_type
+        elif self.data_type == "mXT" and self.mxt_signal_type is not None:
+            self.signal_type = self.mxt_signal_type
         file_dialog = QFileDialog(self)
         file_dialog.setAcceptMode(QFileDialog.AcceptOpen)
         file_dialog.setFileMode(QFileDialog.ExistingFiles)
